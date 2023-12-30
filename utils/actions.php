b2d129be7ebfe824d2d3fedb1a7a45682af8865a
okakaalf <42409609+okakaalf@users.noreply.github.com>
2023-12-30 12:24:15 +0300

    Update actions.php

--- utils/actions.php
+++ utils/actions.php
@@ -4,5 +4,10 @@ class actions{
 	static function log($where,$what){
 		$conn=new db();
 		return $conn->sql("INSERT INTO sys_logs VALUES('',?,?,?,?,?,?)",[$where,$what,session['u'],datestamp,'',ipaddress]);
-	}	
+	}
+	
+	static function message($text,$id='message',$actions='okay',$type='alert',$title=''){
+		global $response;
+		$response['message'][$id]=['title'=>$title,'text'=>$text,'type'=>$type,'actions'=>$actions];
+	}
 }
